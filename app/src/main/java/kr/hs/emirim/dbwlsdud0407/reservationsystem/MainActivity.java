c1aff885b2968a973567fd176258d14b98b47143
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-05-10 19:32:17 +0900

    #2 First Event Programing

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/reservationsystem/MainActivity.java
@@ -2,12 +2,35 @@ package kr.hs.emirim.dbwlsdud0407.reservationsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+    Chronometer chrono;
+    Button butStart,butDone;
+    RadioButton radioDate, radioTime;
+    CalendarView calView;
+    TimePicker timepick;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chrono=(Chronometer)findViewById(R.id.chrono);
+        butStart=(Button)findViewById(R.id.but_start);
+        butDone=(Button)findViewById(R.id.but_done);
+        radioDate=(RadioButton)findViewById(R.id.radio_date);
+        radioTime=(RadioButton)findViewById(R.id.radio_time);
+        calView=(CalendarView)findViewById(R.id.calender);
+        timepick=(TimePicker)findViewById(R.id.time_pick);
+        textResult=(TextView)findViewById(R.id.text_result);
+        timepick.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
     }
 }
