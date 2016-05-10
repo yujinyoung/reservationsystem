6b250e190b6b5864601b9b2d2d4c0a0db1ed5e5f
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-05-10 20:15:35 +0900

    #3 Second Event Programing

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/reservationsystem/MainActivity.java
@@ -32,5 +32,21 @@ public class MainActivity extends AppCompatActivity {
         textResult=(TextView)findViewById(R.id.text_result);
         timepick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);
+
+        radioDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.VISIBLE);
+                timepick.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        radioTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.INVISIBLE);
+                timepick.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
